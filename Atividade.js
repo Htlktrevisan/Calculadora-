0b64a5ba0c52513edd501444d205b45c19912c68
Matheus Trevisan Ribeiro <162151256+Htlktrevisan@users.noreply.github.com>
2024-07-11 09:18:50 -0300

    Add files via upload

---
+++ Atividade.js
@@ -0,0 +1,8 @@
+let elementoInputUm = document.getElementById('inptUm')
+let elementoInputDois = document.getElementById('inptDois')
+
+function SomaNumero(){
+
+    alert(Number(elementoInputUm.value) + Number(elementoInputDois.value))
+
+}
